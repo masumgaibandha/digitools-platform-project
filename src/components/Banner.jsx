0e7fc1c79ab6ae89686b29c76c7ae7a8579a89e7
masumgaibandha <masumgaibandha@gmail.com>
2026-03-31 10:04:12 +0600

    navbar added

---
+++ src/components/Banner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import bannerImg from "../assets/banner.png";
+import playImg from "../assets/Play.png";
+
+const Banner = () => {
+  return (
+    
+  );
+};
+
+export default Banner;
